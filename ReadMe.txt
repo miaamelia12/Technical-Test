72995936c69c98285eebd815b861b1cb355bdd91
Fahmia Amelia <miaamelia940@gmail.com>
2021-10-15 13:56:04 +0700

    technical test diesel one

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Sebelum program dijalankan, install node js terlebih dahulu melalui link berikut https://nodejs.org/en/download/
+
+Jika device sudah ter-install node js maka cara menjalankan program sebagai berikut
+1. Buka terminal yang sudah diarahkan ke folder penyimpanan program
+2. Kemudian ketik node namafile.js contoh "node number2.js"
--- number2.js
+++ number2.js
@@ -1,8 +1,14 @@
 function number2(leaderboards, player) {
-  var result = '';
+  var result = [];
   var merge = leaderboards.concat(player);
   var sort_arr = merge.sort();
-  for ( var i = 0; i < player; i++ ) {
-
+  for ( var i = 0; i < player.length; i++ ) {
+    var index = sort_arr.findIndex(function find(num) {
+      return num == player[i];
+    })
+    result.push(index+1);
   }
+return result;
 }
+
+console.log(number2([200, 100, 100, 50, 10], [20, 80, 80, 10, 10, 300]));
